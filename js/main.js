6b47469954664a6f2e6ee0a9a32516dda25cd03f
pr0d1gy00 <calitoalejandro184@gmail.com>
2023-12-08 16:46:56 -0400

    ultimo codigo funcional, luego de cierto problema

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 const openmodal = document.querySelector('.btn__contacto');
 const modal = document.querySelector('.modal');
 const closemodal = document.querySelector('.modal__close');
+const starcontact = document.querySelector('.contact');
+const contactanimated = document.querySelector('.btn__contact')
+
+
+starcontact.addEventListener('click',(e)=>{
+    e.preventDefault();
+    contactanimated.classList.add('contact--animation');    
+    setTimeout(()=>{
+        contactanimated.classList.remove('contact--animation');
+    }, 3000)
+})
+
 
 
 openmodal.addEventListener('click', (e)=>{
